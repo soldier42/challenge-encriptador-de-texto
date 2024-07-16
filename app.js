72ac33a9682428a74aa852c7f0b787bd6fd7513a
Gabriel Cardoso <79941594+soldier42@users.noreply.github.com>
2024-07-16 00:15:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+function getTextoInput() {
+    // funcao que retorna o texto a ser criptogrado ou descriptografado
+    let textoInput = document.querySelector('textarea').value;
+    return textoInput;
+}
+
+function setResultado(msg) {
+    let boxResultado = document.querySelector('.container__result');
+    let textAreaResultado = criaTextArea(msg, 'container__result__textarea');
+    let botao = criaBotao('Copiar', 'container__result__botao');
+
+    boxResultado.innerHTML = '';
+    boxResultado.style.justifyContent = 'space-between';
+
+    botao.setAttribute('onclick', 'copiar()');
+
+    boxResultado.appendChild(textAreaResultado);
+    boxResultado.appendChild(botao);
+}
+
+function criaTextArea(msg, classe) {
+    let textArea = document.createElement('textarea');
+    textArea.setAttribute('class', classe);
+    textArea.setAttribute('readonly', 'true');
+    textArea.innerHTML = msg.trim();
+    
+    return textArea;
+}
+
+function criaBotao(msg, classe) {
+    let botao = document.createElement('button');
+    botao.setAttribute('class', classe);
+    botao.innerHTML = msg.trim();
+    return botao;
+}
+
+function criptografarTexto() {
+    let textoNormal = getTextoInput();
+    let textoCriptografado = '';
+
+    for(let palavra of textoNormal.split(' ')) { // loop usado para percorrer todas as palavras da frase separadamente
+        for(let char of palavra) {  // loop usado para percorrer e criptografar todos as vogais contidas na palavra
+            switch (char) {
+                case 'a':
+                    textoCriptografado += 'ai';
+                    break;
+                case 'e':
+                    textoCriptografado += 'enter';
+                    break;
+                case 'i':
+                    textoCriptografado += 'imes';
+                    break;
+                case 'o':
+                    textoCriptografado += 'ober';
+                    break;
+                case 'u':
+                    textoCriptografado += 'ufat';
+                    break;
+                default:
+                    textoCriptografado += char;
+            }
+        }
+        textoCriptografado += ' '; // ao final do loop das vogais é adicionado um espaço para separar a proxima palavra
+    }
+
+    setResultado(textoCriptografado);
+    // return textoCriptografado;
+}
+
+function descriptografarTexto() {
+    let textoDescriptografado = getTextoInput();
+
+    // utilizando expressoes regulares, conseguimos substituir todas as ocorrencias que aparecem na frase
+    textoDescriptografado = textoDescriptografado.replace(/ai/g, 'a');
+    textoDescriptografado = textoDescriptografado.replace(/enter/g, 'e');
+    textoDescriptografado = textoDescriptografado.replace(/imes/g, 'i');
+    textoDescriptografado = textoDescriptografado.replace(/ober/g, 'o');
+    textoDescriptografado = textoDescriptografado.replace(/ufat/g, 'u');
+
+    setResultado(textoDescriptografado);
+    // return textoDescriptografado;
+}
+
+
+function copiar() {
+    let texto = document.querySelector('.container__result__textarea').innerHTML;
+    navigator.clipboard.writeText(texto);
+
+    let botao = document.querySelector('.container__result__botao');
+    botao.innerHTML = 'Copiado!';
+    
+}
